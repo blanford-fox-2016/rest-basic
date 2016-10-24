7ef163681ce355d1952845491cfcf63d0ce6ac08
tonychen09 <tony_chen93@yahoo.com>
2016-10-24 15:31:47 +0700

    'rest-basic'

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+var controller = require('../controller/controller')
+/* GET home page. */
+//router.get('/',controller.getResource);
+router.get('/api/users',controller.displayAll);
+router.get('/api/users/:id',controller.displayOne);
+router.post('/api/users',controller.create);
+router.delete('/api/users/:id',controller.delete);
+router.put('/api/users/:id',controller.updateUser);
+
+// router.get('/:id', function(req, res, next) {
+//   diary.destroy({where:{
+//     id : req.params.id
+//   }}).then(res.redirect('/diary'))
+// });
+
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+var controller = require('../controller/controller')
+
+// router.get('/',controller.displayAll);
+// router.post('/',controller.create);
+
+module.exports = router;
